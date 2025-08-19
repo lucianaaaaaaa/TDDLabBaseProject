fdf3ec5d0372bbae70d6f1bb613acfa0a599a66c
lucianaaaaaaa <luciana.leano@ucb.edu.bo>
2025-08-18 23:25:32 -0400

    primer commit

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,5 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
@@ -10,6 +11,8 @@ form.addEventListener("submit", (event) => {
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
+  const sum = sumar(firstNumber, secondNumber);
+  const product = multiplicar(firstNumber, secondNumber);
+  div.innerHTML = `La suma es: ${sum} y el producto es: ${product}`;
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
 });
